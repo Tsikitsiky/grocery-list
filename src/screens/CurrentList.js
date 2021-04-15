a79c8ba28623947b008e0be8bd5a3d628bc13d24
Sarah Razanatsoa <sarah.raz@onja.org>
2021-04-15 11:30:41 +0300

    change into FlatList

--- src/screens/CurrentList.js
+++ src/screens/CurrentList.js
@@ -1,28 +1,44 @@
 import React from 'react'
 
-import { View, Text, SafeAreaView, ScrollView } from 'react-native'
+import { View, Text, SafeAreaView, ScrollView, FlatList } from 'react-native'
 
 import ListItem, { Separator } from '../components/ListItem'
 
 import nachos from '../data/nachos'
 export default () => {
-    return (
+    return(
         <SafeAreaView>
-            <ScrollView>
-            {nachos.map((item, index)=>(
-                <React.Fragment key={item.id}>
-                    <ListItem 
-                    name={item.name} 
-                    onFavouritePress={() => alert('todo: handle favorite')}
-                    isFavourite={index < 2} 
-                    />
-                    <Separator />
-                </React.Fragment>
-            )
+            <FlatList 
+            data={nachos}
+            renderItem={({item, index}) => (
+                <ListItem 
+                name={item.name}
+                onFavouritePress={() => alert('todo: handle favorite')}
+                isFavourite={index < 2} 
+                />
             )}
-            </ScrollView>
+            keyExtractor={(item) => item.id} 
+            ItemSeparatorComponent={() => <Separator />}
+            />
         </SafeAreaView>
-
     )
+    // return (
+    //     <SafeAreaView>
+    //         <ScrollView>
+    //         {nachos.map((item, index)=>(
+    //             <React.Fragment key={item.id}>
+    //                 <ListItem 
+    //                 name={item.name} 
+    //                 onFavouritePress={() => alert('todo: handle favorite')}
+    //                 isFavourite={index < 2} 
+    //                 />
+    //                 <Separator />
+    //             </React.Fragment>
+    //         )
+    //         )}
+    //         </ScrollView>
+    //     </SafeAreaView>
+
+    // )
 
 };
