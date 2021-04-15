dff3172af9e4de2fc2d0ae59c792f9312f6c67af
Sarah Razanatsoa <sarah.raz@onja.org>
2021-04-15 09:53:27 +0300

    add list with separator

--- src/screens/CurrentList.js
+++ src/screens/CurrentList.js
@@ -1,16 +1,22 @@
 import React from 'react'
 
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, ScrollView } from 'react-native'
+
+import ListItem, { Separator } from '../components/ListItem'
 
 import nachos from '../data/nachos'
 export default () => {
     return (
         <SafeAreaView>
-            {nachos.map((item)=>{
-
-                return <Text>{item.name}</Text>
-
-            })}
+            <ScrollView>
+            {nachos.map((item)=>(
+                <React.Fragment key={item.id}>
+                    <ListItem name={item.name} />
+                    <Separator />
+                </React.Fragment>
+            )
+            )}
+            </ScrollView>
         </SafeAreaView>
 
     )
