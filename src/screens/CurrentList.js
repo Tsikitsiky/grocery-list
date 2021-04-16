208bcebdfa2448d8f8e197081eaabd6e8052274a
Sarah Razanatsoa <sarah.raz@onja.org>
2021-04-16 11:50:44 +0300

    add listmanager custum hook, fix add item

--- src/screens/CurrentList.js
+++ src/screens/CurrentList.js
@@ -1,47 +1,14 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 import { View, Text, SafeAreaView, ScrollView, FlatList, KeyboardAvoidingView, ActivityIndicator } from 'react-native'
-import AsyncStorage from '@react-native-community/async-storage';
 import AddItem from '../components/AddItem';
+import {useCurrentList} from '../util/ListManager'
 
-//import uuid from 'uuid/v4';
-
-import {v4 as uuid } from 'uuid';
 
 import ListItem, { Separator } from '../components/ListItem'
 
-//import nachos from '../data/nachos'
-
-const updateStoredCurrentList = (list) => {
-    AsyncStorage.setItem('@@GroceryList/currentList', JSON.stringify(list));
-}
 export default () => {
-    const [list, setList] = useState([]);
-    const [loading, setLoading] = useState(true);
-
-    const addItem = (text) => {
-        const newList = [{id: uuid(), name: text}, ...list];
-        setList(newList);
-        updateStoredCurrentList(newList)
-    }
-    const removeItem = (id) => {
-        const newList = list.filter(item => item.id !== id);
-        setList(newList);
-        updateStoredCurrentList(newList)
-    }
-
-    useEffect(() => {
-        setTimeout(() => {
-            AsyncStorage.getItem('@@GroceryList/currentList')
-        .then(data => JSON.parse(data))
-        .then(data => {
-            if(data) {
-                setList(data)
-            }
-            setLoading(false);
-        })
-        }, 1000);
-    }, []);
+    const {list, loading, addItem, removeItem} = useCurrentList();
 
 if(loading) {
     return (
